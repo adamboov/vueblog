c6c4b3299cb814509e1d73c0a15fa44d4eea609a
adamboov <48423629+adamboov@users.noreply.github.com>
2020-06-03 00:06:36 +0800

    拦截器跨域配置

--- src/main/java/com/adam/vueblog/shiro/JwtFilter.java
+++ src/main/java/com/adam/vueblog/shiro/JwtFilter.java
@@ -11,9 +11,11 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.ExpiredCredentialsException;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.apache.shiro.web.util.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -77,5 +79,22 @@ public class JwtFilter extends AuthenticatingFilter {
         }
 
         return false;
+    }
+
+    @Override
+    protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
+
+        HttpServletRequest httpServletRequest = WebUtils.toHttp(request);
+        HttpServletResponse httpServletResponse = WebUtils.toHttp(response);
+        httpServletResponse.setHeader("Access-controller-Allow-Origin", httpServletRequest.getHeader("Origin"));
+        httpServletResponse.setHeader("Access-Controller-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
+        httpServletResponse.setHeader("Access-Controller-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
+
+        //  跨域时会首先发送一个options请求，这里我们给options请求直接返回正常状态
+        if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            httpServletResponse.setStatus(org.springframework.http.HttpStatus.OK.value());
+            return false;
+        }
+        return super.preHandle(request, response);
     }
 }
